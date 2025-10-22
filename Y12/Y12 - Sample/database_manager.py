5ffe9cb096665c9190f20a26a8c2ac3a3e4274db
Pieskyy <157950959+Pieskyy@users.noreply.github.com>
2025-10-22 12:24:38 +1100

    Got images. fixed database to have the new images. started moving template files in

--- Y12/Y12 - Sample/database_manager.py
+++ Y12/Y12 - Sample/database_manager.py
@@ -2,16 +2,8 @@ import sqlite3 as sql
 
 
 def listExtension():
-    con = sql.connect("database/data_source.db")
+    con = sql.connect("database/cards.db")
     cur = con.cursor()
     data = cur.execute("SELECT * FROM extension").fetchall()
     con.close()
     return data
-
-
-def insertContact(email, name):
-    con = sql.connect("database/data_source.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO contact_list (email,name) VALUES (?,?)", (email, name))
-    con.commit()
-    con.close()
