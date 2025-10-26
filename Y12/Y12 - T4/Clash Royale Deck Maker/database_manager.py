ff90d8e7826b6d589e349134cc477f57abde0c06
Pieskyy <wilkieadam12@gmail.com>
2025-10-26 18:56:03 +1100

    removed pointless part

--- Y12/Y12 - T4/Clash Royale Deck Maker/database_manager.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-def listExtension():
-    con = sql.connect("database/cards.db")
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM cards").fetchall()  # <-- change to your actual table name
-    con.close()
-    return data
